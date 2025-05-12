f0453162683a95c0943e4583c83710963720d871
Matheus Santana <matheusfsantana99@gmail.com>
2025-05-12 19:22:55 -0300

    Add java

---
+++ services/stock-service/src/test/java/br/com/product_stock_monorepo/stock_service/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.product_stock_monorepo.stock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
